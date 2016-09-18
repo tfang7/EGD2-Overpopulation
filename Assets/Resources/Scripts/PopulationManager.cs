d98257d2272bb36801ffc5b5174b44f4ec1340c5
DESKTOP-7OOGRRA\dkfan <dkfang7@gmail.com>
2016-09-18 14:47:42 -0400

    added base layout for main game scene. some random population growth creator and panels for population stats.

---
+++ Assets/Resources/Scripts/PopulationManager.cs
@@ -0,0 +1,67 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class PopulationManager : MonoBehaviour {
+
+
+    private Text populationText;
+    private Text deathRateText;
+    private Text birthRateText;
+    private Text growthRateText;
+
+    public float  birthRate;
+    public float deathRate;
+    public float growthRate;
+
+    public int totalPopulation;
+    public int workPopulation;
+    
+	// Use this for initialization
+	void Start () {
+        totalPopulation = 100;
+        birthRate = 100;
+        deathRate = 0;
+        growthRate = 0f;
+        populationText = GameObject.Find("populationCounter").GetComponent<Text>();
+        deathRateText = GameObject.Find("deathRate").GetComponent<Text>();
+        birthRateText = GameObject.Find("birthRate").GetComponent<Text>();
+        growthRateText = GameObject.Find("growthRate").GetComponent<Text>();
+    }
+    // Update is called once per frame
+    void Update () {
+        birthRate = adjustRate(birthRate, Random.Range(1,100) * 1/3);
+        deathRate += 10;
+        Debug.Log(birthRate);
+        calculateGrowthRate();
+        totalPopulation += (int)Mathf.Ceil(growthRate * Time.deltaTime);
+        setText();
+        // Debug.Log(totalPopulation);
+    }
+    void setPopulationText()
+    {
+            
+    }
+    float adjustRate(float rate, float change) {
+        rate += change;
+        return rate;
+    }
+    void setText()
+    {
+        populationText.text = totalPopulation.ToString();
+        birthRateText.text = "Birth Rate:" + birthRate.ToString();
+        deathRateText.text = "Death Rate:" + deathRate.ToString();
+    }
+    void calculateGrowthRate()
+    {
+        growthRate = birthRate - deathRate;
+        growthRateText.color = Color.red;
+        if (growthRate > 0)
+        {
+            growthRateText.color = Color.green;
+        }
+        growthRateText.text = "Growth Rate: " + growthRate.ToString();
+        
+    }
+
+}
---
+++ Assets/Resources/Scripts/StatManager.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class StatManager : MonoBehaviour {
+    public Image[] populationStats;
+    private float fillRate = 0.1f;
+	// Use this for initialization
+	void Start () {
+        for (int i = 0; i < populationStats.Length; i++)
+        {
+            populationStats[i].fillAmount -= fillRate * Time.deltaTime;
+        }
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Image statImage;
+        for (int i = 0; i < populationStats.Length; i++)
+        {
+            statImage = populationStats[i];
+
+            if (statImage.fillAmount <= 0f) statImage.fillAmount = 1f;
+
+            statImage.fillAmount -= fillRate * Time.deltaTime;
+            
+            if (statImage.GetComponentInChildren<Text>()) statImage.GetComponentInChildren<Text>().text = (Mathf.RoundToInt( Mathf.Clamp01(populationStats[i].fillAmount) * 100)).ToString();
+
+        }
+    }
+}
