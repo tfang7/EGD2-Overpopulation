7acfb0e4f119277d7c09ada8c7286bfc3601a0c4
Philip Cioni <philcioni@gmail.com>
2016-09-20 17:23:46 -0400

    no message

--- Assets/Resources/Scripts/GameManager.cs
+++ Assets/Resources/Scripts/GameManager.cs
@@ -8,21 +8,47 @@ public class GameManager : MonoBehaviour {
     PopulationManager populationManager;
     public int tickrate = 1;
 
+    float v = 0f;
+    bool buttonsActive = false;
+
     public GameObject[] buttons;
 
 	// Use this for initialization
 	void Start () {
         populationManager = populationController.GetComponent<PopulationManager>();
         StartCoroutine("Tick");
-	}
-	
-	// Update is called once per frame
-	void Update () {
+        GenerateButtons();
+    }
+
+    // Update is called once per frame
+    void Update () {
 	
 	}
 
+    IEnumerator GenerateButtons() {
+        Debug.Log("generating buttons");
+        if (buttonsActive) {
+            foreach (GameObject button in buttons) {
+                float newPos = Mathf.SmoothDamp(button.transform.position.y, button.transform.position.y - 50, ref v, .2f);
+                button.transform.position = new Vector3(button.transform.position.x, newPos, button.transform.position.z);
+            }
+            foreach (GameObject button in buttons) {
+                float newPos = Mathf.SmoothDamp(button.transform.position.y, button.transform.position.y + 50, ref v, .2f);
+                button.transform.position = new Vector3(button.transform.position.x, newPos, button.transform.position.z);
+            }
+        }
+        else {
+            foreach (GameObject button in buttons) {
+                float newPos = Mathf.SmoothDamp(button.transform.position.y, button.transform.position.y + 50, ref v, .2f);
+                button.transform.position = new Vector3(button.transform.position.x, newPos, button.transform.position.z);
+            }
+        }
+    }
+
     IEnumerator Tick() {
+        int ticker = 0;
         while (true) {
+            ticker++; 
             Debug.Log("tick");
             TickSkills();
             populationManager.Tick();
